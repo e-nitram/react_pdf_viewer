0471661e27686be19a4c61d037f678c87af2290a
e-nitram <sammy.jam.robinson@gmail.com>
2023-08-24 09:36:41 -0400

    complete react_pdf_viewer

---
+++ packages/bookmark/src/index.ts
@@ -0,0 +1,13 @@
+/**
+ * A React component to view a PDF document
+ *
+ * @see https://react-pdf-viewer.dev
+ * @license https://react-pdf-viewer.dev/license
+ * @copyright 2019-2023 Nguyen Huu Phuoc <me@phuoc.ng>
+ */
+
+export { DownArrowIcon } from './DownArrowIcon';
+export { RightArrowIcon } from './RightArrowIcon';
+export * from './bookmarkPlugin';
+export { type IsBookmarkExpanded } from './types/IsBookmarkExpanded';
+export { type RenderBookmarkItem, type RenderBookmarkItemProps } from './types/RenderBookmarkItemProps';
---
+++ packages/selection-mode/src/index.ts
@@ -0,0 +1,14 @@
+/**
+ * A React component to view a PDF document
+ *
+ * @see https://react-pdf-viewer.dev
+ * @license https://react-pdf-viewer.dev/license
+ * @copyright 2019-2023 Nguyen Huu Phuoc <me@phuoc.ng>
+ */
+
+export { HandToolIcon } from './HandToolIcon';
+export { type SwitchSelectionModeProps } from './SwitchSelectionMode';
+export { TextSelectionIcon } from './TextSelectionIcon';
+export * from './selectionModePlugin';
+export { SelectionMode } from './structs/SelectionMode';
+export { type RenderSwitchSelectionModeProps } from './types/RenderSwitchSelectionModeProps';
